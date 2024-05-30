afc7c45e258574393c1de2998dc3a48e514e8d04
Lunar Eclipse, The Trendsetting Hopper <sermkiatv@hotmail.com>
2024-05-31 01:53:53 +0700

    Section 8: Added example script and linked it from index.html. Added some to do items.

---
+++ js/testimony-carousel.js
@@ -0,0 +1,19 @@
+let currentTestimony = 0;
+const testimonies = document.querySelectorAll('.testimony');
+const dots = document.querySelectorAll('.dot');
+
+document.querySelector('.prev').addEventListener('click', () => {
+  testimonies[currentTestimony].style.display = 'none';
+  dots[currentTestimony].style.backgroundColor = '#bbb';
+  currentTestimony = currentTestimony - 1 < 0 ? testimonies.length - 1 : currentTestimony - 1;
+  testimonies[currentTestimony].style.display = 'block';
+  dots[currentTestimony].style.backgroundColor = '#717171';
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  testimonies[currentTestimony].style.display = 'none';
+  dots[currentTestimony].style.backgroundColor = '#bbb';
+  currentTestimony = (currentTestimony + 1) % testimonies.length;
+  testimonies[currentTestimony].style.display = 'block';
+  dots[currentTestimony].style.backgroundColor = '#717171';
+});
